3ef992a0ddad05e15d62df92c419215e2570edef
Arjav <1971arjavbarya@gmail.com>
2023-11-01 23:59:51 +0530

    removed default from select

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -156,7 +156,7 @@ const OurStore = () => {
                     <p className='mb-0 d-block' style={{width:"100px"}}>Sort By</p>
                     <select name="" className='form-control form-select' id="">
                       <option value="manual">Featuerd</option>
-                      <option value="best-selling" selected="selected">Best selling</option>
+                      <option value="best-selling" >Best selling</option>
                       <option value="title-ascending">Alphabetically, A-Z</option>
                       <option value="title-descending">Alphabetically, Z-A</option>
                       <option value="price-ascending">Price , low to high</option>
